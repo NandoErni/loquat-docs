b1bfd919d0e47d6e64b5b295b6f266767966cb5e
nando <nando.erni@outlook.de>
2021-06-12 01:25:04 +0200

    add some more garbage

--- LoquatDocs/MainPage.xaml.cs
+++ LoquatDocs/MainPage.xaml.cs
@@ -1,36 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-namespace LoquatDocs
-{
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
+namespace LoquatDocs {
+  public sealed partial class MainPage : Page {
+    public MainPage() {
+      InitializeComponent();
     }
+
+    private void SaveButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void CancelButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void Button_Click_2(object sender, RoutedEventArgs e) {
+
+    }
+  }
 }
